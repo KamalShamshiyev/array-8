f5352b5fa796778278e5f4449c68afba565cefff
KamalShamsiyev <ksmsiyev@gmail.com>
2022-07-04 12:48:16 +0400

    finish

---
+++ Kamal.js
@@ -0,0 +1,15 @@
+function calcTip(number) {
+    if (number >=  50) if(number <= 300){
+        return number * 15 / 100
+    } 
+    if(number < 50){
+        console.log(number * 20 / 100)
+    }
+}
+console.log(calcTip(prompt("eded daxil din")))
+
+
+ 
+
+
+
