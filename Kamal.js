99b8f828a9b1beace0593d1f25134f3d0c7c2dda
KamalShamsiyev <ksmsiyev@gmail.com>
2022-07-04 13:26:02 +0400

    finish

--- Kamal.js
+++ Kamal.js
@@ -1,15 +1,15 @@
 function calcTip(number) {
-    if (number >=  50) if(number <= 300){
+    if (number >= 50 && number <= 300){
         return number * 15 / 100
     } 
-    if(number < 50){
-        console.log(number * 20 / 100)
+    if(number < 50 || number > 300){
+        return number * 20 / 100
     }
 }
-console.log(calcTip(prompt("eded daxil din")))
+const amount = [44, 555, 150]
+console.log(calcTip(amount[0]))
+console.log(calcTip(amount[1]))
+console.log(calcTip(amount[2]))
 
 
- 
-
-
-
+//console.log(amount[0], amount[1], amount[2])
